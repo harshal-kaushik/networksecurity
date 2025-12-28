4013f91e931f0eacb44379418ff557d33a5cdd51
harshal-kaushik <kaushikharshal02@gmail.comgit config --global user.email kaushikharshal02@gmail.com>
2025-12-28 16:29:46 +0530

    ETL pipline completed

---
+++ test_mongodb.py
@@ -0,0 +1,16 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from urllib.parse import quote_plus
+
+username = "kaushik_user_1"
+password = "PASSWORD"
+
+uri = f"mongodb+srv://{username}:{password}@cluster0.xilgv3j.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(uri, server_api=ServerApi("1"))
+
+try:
+    client.admin.command("ping")
+    print("✅ Connected successfully to MongoDB Atlas!")
+except Exception as e:
+    print("❌ Connection failed:", e)
